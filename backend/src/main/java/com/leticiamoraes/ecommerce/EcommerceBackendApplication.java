26e2041662f7a8a53901d434478ae69fcd9ce901
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2025-03-30 12:00:00 -0300

    feat: Configured the connection with database

--- backend/src/main/java/com/leticiamoraes/ecommerce/EcommerceBackendApplication.java
+++ backend/src/main/java/com/leticiamoraes/ecommerce/EcommerceBackendApplication.java
@@ -1,4 +1,12 @@
 package com.leticiamoraes.ecommerce;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class EcommerceBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceBackendApplication.class, args);
+    }
 }
