a02de57a243e0bdceddcfadd62ad1c397e905105
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2025-04-03 12:00:00 -0300

    feat: Added getters and setters in user entity

--- backend/src/main/java/com/leticiamoraes/ecommerce/model/User.java
+++ backend/src/main/java/com/leticiamoraes/ecommerce/model/User.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 @Table(name = "users")
 public class User {
 
-	@id
+	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
@@ -27,5 +27,57 @@ public class User {
 	private LocalDateTime updated_at;
 
 	public User() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public UserRole getRole() {
+		return role;
+	}
+
+	public void setRole(UserRole role) {
+		this.role = role;
+	}
+
+	public LocalDateTime getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(LocalDateTime created_at) {
+		this.created_at = created_at;
+	}
+
+	public LocalDateTime getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(LocalDateTime updated_at) {
+		this.updated_at = updated_at;
 	}
 }
