d95aa6509e2bd47213c7f858d419a147cf5bca44
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2025-04-06 12:00:00 -0300

    feat: Added getters and setters in category entity

--- backend/src/main/java/com/leticiamoraes/ecommerce/model/Category.java
+++ backend/src/main/java/com/leticiamoraes/ecommerce/model/Category.java
@@ -18,5 +18,23 @@ public class Category {
 	public Category() {
 	}
 
+	public Long getId() {
+		return id;
+	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
