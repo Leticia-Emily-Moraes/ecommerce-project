00f655baefbde6cef70b5a962eea38cd4ce7fb7b
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2025-04-08 12:00:00 -0300

    feat: Added getters and setters in product entity

--- backend/src/main/java/com/leticiamoraes/ecommerce/model/Product.java
+++ backend/src/main/java/com/leticiamoraes/ecommerce/model/Product.java
@@ -30,4 +30,63 @@ public class Product {
 	public Product() {
 	}
 
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public Integer getStock_quantity() {
+		return stock_quantity;
+	}
+
+	public void setStock_quantity(Integer stock_quantity) {
+		this.stock_quantity = stock_quantity;
+	}
+
+	public Long getCategory_id() {
+		return category_id;
+	}
+
+	public void setCategory_id(Long category_id) {
+		this.category_id = category_id;
+	}
+
+	public LocalDateTime getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(LocalDateTime created_at) {
+		this.created_at = created_at;
+	}
+
+	public LocalDateTime getUpdated_at() {
+		return updated_at;
+	}
+
+	public void setUpdated_at(LocalDateTime updated_at) {
+		this.updated_at = updated_at;
+	}
 }
