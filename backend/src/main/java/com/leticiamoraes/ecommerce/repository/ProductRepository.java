df2c70d728c5ed4d096d806239bdcf5ae895b137
leticia-emily-moraes <leticiaemilymoraes14@gmail.com>
2025-04-15 12:00:00 -0300

    feat: Creating the product repository

---
+++ backend/src/main/java/com/leticiamoraes/ecommerce/repository/ProductRepository.java
@@ -0,0 +1,14 @@
+package com.leticiamoraes.ecommerce.repository;
+
+import com.leticiamoraes.ecommerce.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+	Optional<Product> findByName(String pName);
+
+	List<Product> findByCategory_Name(String categoryName);
+}
